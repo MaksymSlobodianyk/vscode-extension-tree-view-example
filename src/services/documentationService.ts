6cdf8f6bacc03977f46f1e44fadc956c92563e37
MaksymSlobodianyk <maxslobodianyk@gmail.com>
2021-11-28 22:00:56 +0200

    Fix fetch link

--- src/services/documentationService.ts
+++ src/services/documentationService.ts
@@ -2,6 +2,6 @@ import fetch from 'node-fetch'
 import { RootNode } from '../types'
 
 export const getDocumentationConfig = async (): Promise<RootNode> => {
-    const response = await fetch('https://maksymslobodianyk.github.io/vscode-extension-tree-view-example/data.json');
+    const response = await fetch('https://maksymslobodianyk.github.io/vscode-extension-tree-view-example/data/data.json');
     return await response.json();
 }
